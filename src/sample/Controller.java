f430bdde42ce60644ba51c6e0a19de1c07c4f00c
AntonZS <imbaRA12>
2017-01-29 13:20:04 +0300

    new button ang textField+set/get style

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -54,6 +54,54 @@ public class Controller {
     Lab_SB space1 = new Lab_SB(Dxx);
 
 
+    double[] LAB = new double[3];
+
+    public double[] getLAB() {
+        LAB[0] = Double.parseDouble(tfL.getText());
+        LAB[1] = Double.parseDouble(tfa.getText());
+        LAB[2] = Double.parseDouble(tfb.getText());
+        return LAB;
+    }
+
+    public void setLAB(double[] LAB) {
+        this.LAB = LAB;
+    }
+
+    public double[] getXYZ() {
+        return XYZ;
+    }
+
+    public void setXYZ(double[] XYZ) {
+        tfXYZ_X.setText(String.valueOf(XYZ[0]));
+        tfXYZ_Y.setText(String.valueOf(XYZ[1]));
+        tfXYZ_Z.setText(String.valueOf(XYZ[2]));
+    }
+
+    public int[] getHSB() {
+        return HSB;
+    }
+
+    public void setHSB(int[] HSB) {
+        tfH.setText(String.valueOf(HSB[0]));
+        tfS.setText(String.valueOf(HSB[1]));
+        tfB.setText(String.valueOf(HSB[2]));
+    }
+
+    public int[] getRGB() {
+        return RGB;
+    }
+
+    public void setRGB(int[] RGB) {
+        tfRGB_R.setText(String.valueOf(RGB[0]));
+        tfRGB_G.setText(String.valueOf(RGB[1]));
+        tfRGB_B.setText(String.valueOf(RGB[2]));
+    }
+
+    double[] XYZ = new double[3];
+    int[] HSB = new int[3];
+    int[] RGB = new int[3];
+
+
     @FXML
     public void initialize(){
         btnLAB.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
@@ -66,19 +114,18 @@ public class Controller {
                         !tfa.getText().isEmpty() &&
                         !tfb.getText().isEmpty()) {
 
-                    double L = Double.parseDouble(tfL.getText());
-                    double a = Double.parseDouble(tfa.getText());
-                    double b = Double.parseDouble(tfb.getText());
+                    LAB = getLAB();
 
-                    double[] Lab = {L, a, b};
-                    int[] HSB = space1.LABtoHSB(Lab);
-                    int[] RGB = space1.LABtoRGB(Lab);
-                    if (RGB[0] < 255 && RGB[1] < 255 && RGB[2] < 255) {
+                    RGB = space1.LABtoRGB(LAB);
+                    if (RGB[0] < 255 && RGB[1] < 255 && RGB[2] < 255 && RGB[0] > 0 && RGB[1] > 0 && RGB[2] > 0) {
                         lout1.setText("");
+
                         ap2.setBackground(new Background(new BackgroundFill(Color.rgb(RGB[0], RGB[1], RGB[2]), CornerRadii.EMPTY, Insets.EMPTY)));
-                        tfH.setText(String.valueOf(HSB[0]));
-                        tfS.setText(String.valueOf(HSB[1]));
-                        tfB.setText(String.valueOf(HSB[2]));
+
+                        setRGB(space1.LABtoRGB(LAB));
+                        setXYZ(space1.LABtoXYZ(LAB));
+                        setHSB(space1.LABtoHSB(LAB));
+
                     } else {
                         lout1.setText("этот цвет не кодируется в sRGB и HSB");
                         ap2.setBackground(new Background(new BackgroundFill(Color.rgb(255, 255, 255), CornerRadii.EMPTY, Insets.EMPTY)));
